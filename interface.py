f98b04db14f48ac67f9789f03792ab95979df977
userklay <arqkdias@gmail.com>
2022-12-18 09:04:53 -0400

    -segunda mudança por hj

---
+++ interface.py
@@ -0,0 +1,29 @@
+import PySimpleGUI as sg
+import  funcao 
+
+class interface:
+    def __init__(self):
+        #tela inicial 
+        sg.theme('Reddit')
+        layout_login = [
+            [sg.Output(size=(15,1))],
+            [sg.Btn('7',size=(2,1)),sg.Btn('8',size=(2,1)),sg.Btn('9',size=(2,1)),sg.Btn('/',size=(2,1))],
+            [sg.Btn('4',size=(2,1)),sg.Btn('5',size=(2,1)),sg.Btn('6',size=(2,1)),sg.Btn('x',size=(2,1))],
+            [sg.Btn('1',size=(2,1)),sg.Btn('2',size=(2,1)),sg.Btn('3',size=(2,1)),sg.Btn('-',size=(2,1))],
+            [sg.Btn(',',size=(2,1)),sg.Btn('0',size=(2,1)),sg.Btn('=',size=(2,1)),sg.Btn('+',size=(2,1))],
+            ]
+
+        window = sg.Window('Calculadora',layout=layout_login, finalize = True)
+        
+        
+        while True:
+            event, values = window.read()    
+            if event == sg.WIN_CLOSED:
+                break
+            
+            
+            
+            window.close()
+        
+interface()
+
