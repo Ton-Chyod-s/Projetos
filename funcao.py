95275a59d9d9da94b3f0396b87a561e3e204263f
userklay <arqkdias@gmail.com>
2022-12-19 21:04:49 -0400

    -mudança simples

--- funcao.py
+++ funcao.py
@@ -1,26 +1,11 @@
-class calc:
-	def __init__(self, a, b):
-		self.a = a 
-		self.b = b
-		
-	def soma(self):
-		c = self.a + self.b
-		return print(c)
-	
-	def sub(self):
-		c = self.a - self.b
-		return print(c)
-	
-	def mult(self):
-		c = self.a * self.b
-		return print(c)
- 	
-	def div(self):
-		c = self.a / self.b
-		return print(c)
+#def para fazer calculos matematicos +, - , * , /
+def calc(arg):
+	usuario = str(arg)
+	conta = eval(usuario)
+	print(f'o resultado é:\t{conta}')
 
 
 if __name__ == '__main__':
-		num = calc(5,2)
-		num.div()
+	calc(5 * 2)
+	
 		
