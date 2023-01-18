8cd05a5c3df7bbb2cdd438d31d04534caf7a885b
userklay <arqkdias@gmail.com>
2023-01-17 20:42:40 -0400

    -teste com a biblioteca pywhatkit

--- main_bot.py
+++ main_bot.py
@@ -3,3 +3,11 @@ import keyboard
 import time
 from datetime import datetime
 
+contato = ['+5567991799956']
+
+while len(contato) >= 1:
+    # Send a WhatsApp Message to a Contact at 1:30 PM
+    pywhatkit.sendwhatmsg(contato[0], "um teste ae", datetime.now().hour, datetime.now().minute + 1)
+    del contato[0]
+    time.sleep(1)
+    keyboard.press_and_release('ctrl + w')
