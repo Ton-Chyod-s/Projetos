d82d2f1f2e24bc5b7eca428cb420a9996ce6527d
userklay <arqkdias@gmail.com>
2023-01-17 21:10:14 -0400

    -criando GUI

--- main_bot.py
+++ main_bot.py
@@ -2,7 +2,27 @@ import pywhatkit
 import keyboard
 import time
 from datetime import datetime
+import PySimpleGUI as sg
 
+sg.theme('DarkAmber')
+
+layout = [  [sg.Text('Contatos'), sg.InputText()],
+            [sg.Text('Mensagem'), sg.InputText()],
+            [sg.Button('Ok'), sg.Button('Cancel')] ]
+
+# Create the Window
+window = sg.Window('Bot Whatsapp', layout)
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+    print('You entered ', values[0])
+
+window.close()
+
+
+'''
 contato = ['+5567991799956']
 
 while len(contato) >= 1:
@@ -11,3 +31,4 @@ while len(contato) >= 1:
     del contato[0]
     time.sleep(5)
     keyboard.press_and_release('ctrl + w')
+'''
