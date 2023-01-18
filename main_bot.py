088343305c28042d5f6c0502ca7f5c6a2f4fd51d
userklay <arqkdias@gmail.com>
2023-01-17 20:55:49 -0400

    -lol

--- main_bot.py
+++ main_bot.py
@@ -7,7 +7,7 @@ contato = ['+5567991799956']
 
 while len(contato) >= 1:
     # Send a WhatsApp Message to a Contact 
-    pywhatkit.sendwhats_image(contato[0],'', "um teste ae", datetime.now().hour, datetime.now().minute + 1)
+    pywhatkit.sendwhats_image(contato[0],'1662693303800 (1).jpg', "um teste ae", datetime.now().hour, datetime.now().minute + 1)
     del contato[0]
-    time.sleep(1)
+    time.sleep(5)
     keyboard.press_and_release('ctrl + w')
