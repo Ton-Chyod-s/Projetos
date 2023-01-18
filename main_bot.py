2415bb4a920b3706fedb855480547749dbd35821
userklay <arqkdias@gmail.com>
2023-01-17 20:48:37 -0400

    -lol

--- main_bot.py
+++ main_bot.py
@@ -6,8 +6,8 @@ from datetime import datetime
 contato = ['+5567991799956']
 
 while len(contato) >= 1:
-    # Send a WhatsApp Message to a Contact at 1:30 PM
-    pywhatkit.sendwhatmsg(contato[0], "um teste ae", datetime.now().hour, datetime.now().minute + 1)
+    # Send a WhatsApp Message to a Contact 
+    pywhatkit.sendwhats_image(contato[0],'', "um teste ae", datetime.now().hour, datetime.now().minute + 1)
     del contato[0]
     time.sleep(1)
     keyboard.press_and_release('ctrl + w')
