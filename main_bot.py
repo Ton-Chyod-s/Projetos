5a84041615d6492503e9fd471a7dd1b6404a3213
userklay <arqkdias@gmail.com>
2023-01-23 21:46:50 -0400

    -configurando bot

--- main_bot.py
+++ main_bot.py
@@ -17,33 +17,33 @@ def endereco(procurar = True):
         aplicativo()
         
 class bot:
-    def __init__(self):
-        self.contato = ['+5567991799956']
-        self.imagem = '1662693303800 (1).jpg'
+    def __init__(self, contato, mensagem):
+        self.contato = contato
+        self.mensagem = mensagem
 
-        def mensagem_imagem(self):
-            while len(self.contato) >= 1:
-                # Send a WhatsApp Message to a Contact 
-                pywhatkit.sendwhats_image(self.contato[0],self.imagem, "um teste ae", datetime.now().hour, datetime.now().minute + 1)
-                del self.contato[0]
-                time.sleep(5)
-                keyboard.press_and_release('ctrl + w')
+    def mensagem_imagem(self):
+        while len(self.contato) >= 1:
+            # Send a WhatsApp Message to a Contact 
+            pywhatkit.sendwhats_image(self.contato[0],self.imagem, "um teste ae", datetime.now().hour, datetime.now().minute + 1)
+            del self.contato[0]
+            time.sleep(5)
+            keyboard.press_and_release('ctrl + w')
 
-        def mensagem_texto(self):
-            while len(self.contato) >= 1:
-                # Send a WhatsApp Message to a Contact at 1:30 PM
-                pywhatkit.sendwhatmsg(self.contato[0], "um teste ae", datetime.now().hour, datetime.now().minute + 1)
-                del self.contato[0]
-                time.sleep(5)
-                keyboard.press_and_release('ctrl + w')
+    def mensagem_texto(self):
+        while len(self.contato) >= 1:
+            # Send a WhatsApp Message to a Contact at 1:30 PM
+            pywhatkit.sendwhatmsg(self.contato[0], "um teste ae", datetime.now().hour, datetime.now().minute + 1)
+            del self.contato[0]
+            time.sleep(5)
+            keyboard.press_and_release('ctrl + w')
 
-        def mensagem_grupo(self):
-            while len(self.contato) >= 1:
-                # Send a WhatsApp Message to a Contact at 1:30 PM
-                pywhatkit.sendwhatmsg_to_group(self.contato[0], "um teste ae", datetime.now().hour, datetime.now().minute + 1)
-                del self.contato[0]
-                time.sleep(5)
-                keyboard.press_and_release('ctrl + w')
+    def mensagem_grupo(self):
+        while len(self.contato) >= 1:
+            # Send a WhatsApp Message to a Contact at 1:30 PM
+            pywhatkit.sendwhatmsg_to_group(self.contato[0], "um teste ae", datetime.now().hour, datetime.now().minute + 1)
+            del self.contato[0]
+            time.sleep(5)
+            keyboard.press_and_release('ctrl + w')
 
 class aplicativo:
     def __init__(self):
@@ -55,7 +55,7 @@ class aplicativo:
             #[sg.Text(size=(16,5))],
             [sg.Checkbox('Com imagem',key='img'),sg.Checkbox('Grupo',key='grp')],
             [sg.Checkbox('Sem imagem',key='N/img')],
-            [sg.Button('Enviar'), sg.Button('Sair'), sg.Button('Test')] 
+            [sg.Button('Enviar'), sg.Button('Sair'), sg.Button('Att Msg')] 
             ]
 
         # Create the Window
@@ -82,7 +82,7 @@ class aplicativo:
 
                 if event == 'Ok':
                     dados = {
-                            "Contato_1": '+55' + values['numero']
+                            "Contato": values['numero']
                         }
                     with open("contatos.json", 'w') as file:
                         json.dump(dados, file, indent=4)
@@ -115,7 +115,7 @@ class aplicativo:
                     dados = {
                             "msg": values['mensagem']
                         }
-                    with open("credenciais.json", 'w') as file:
+                    with open("mensagem.json", 'w') as file:
                         json.dump(dados, file, indent=4)
 
                     window.close()
@@ -140,14 +140,29 @@ class aplicativo:
             if event == 'Mensagem' :
                 aplicativo_window.close()
                 mensagem()
+            
+            if event == 'Att Msg':
+                nav = bot()
                 
             if event == 'Enviar':
-                if values['img']:
-                    pass
+                with open("contatos.json", encoding='utf-8') as meu_json:
+                        dado = json.load(meu_json)
+                msg = dado['Contato']
+                
+                with open("mensagem.json", encoding='utf-8') as meu_json:
+                        dado = json.load(meu_json)
+                contatos = dado['msg']
+                
+                nav = bot(msg,contatos)
+                
+                if values['img'] and values['grp']:
+                    nav.mensagem_imagem()
                     
-
+                elif values['N/img'] and values['grp']:
+                    nav.mensagem_grupo()
+                    
         aplicativo_window.close()
 
 if __name__ == '__main__':
     aplicativo()
-    
+
