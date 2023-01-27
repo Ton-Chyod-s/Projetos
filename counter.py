302a7ee8397b76828f2e64eda6a8da8a0dfb9077
Klay <arqkdias@gmail.com>
2023-01-27 08:24:49 -0400

    -lol

--- counter.py
+++ counter.py
@@ -1,42 +1,10 @@
 import flet as ft
 
-def main(page: ft.Page):
-    page.add(
-        ft.DataTable(
-            columns=[
-                ft.DataColumn(ft.Text("First name")),
-                ft.DataColumn(ft.Text("Last name")),
-                ft.DataColumn(ft.Text("Age"), numeric=True),
-                ],
-            rows=[
-                ft.DataRow(
-                    cells=[
-                        ft.DataCell(ft.Text("John")),
-                        ft.DataCell(ft.Text("Smith")),
-                        ft.DataCell(ft.Text("43")),
-                    ],
-                ),
-                ft.DataRow(
-                    cells=[
-                        ft.DataCell(ft.Text("Jack")),
-                        ft.DataCell(ft.Text("Brown")),
-                        ft.DataCell(ft.Text("19")),
-                    ],
-                ),
-                ft.DataRow(
-                    cells=[
-                        ft.DataCell(ft.Text("Alice")),
-                        ft.DataCell(ft.Text("Wong")),
-                        ft.DataCell(ft.Text("25")),
-                    ],
-                ),
-            ],
-        ),
-    )
 
-    ft.ResponsiveRow([
-    ft.Column(col={"sm": 6}, controls=[ft.Text("Column 1")]),
-    ft.Column(col={"sm": 6}, controls=[ft.Text("Column 2")])
-])
+def main(page: ft.Page):
+    page.title = "to testando"
+    page.add(
+        ft.OutlinedButton(text="Print",on_long_press=print('lol')),
+    )
 
 ft.app(target=main)
