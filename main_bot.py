874bb7afcbef867b219154cdcc1b31043038fbca
userklay <arqkdias@gmail.com>
2023-01-29 14:43:25 -0400

    -ta indo

--- main_bot.py
+++ main_bot.py
@@ -1,1 +1,19 @@
-import selenium
+from selenium.webdriver import Chrome
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+import time
+
+class bot:
+    def __init__(self):
+        pass
+    def navegador(self):
+        self.driver = Chrome()
+    def site_whatsapp(self):
+         self.driver.get("https://web.whatsapp.com/")
+    
+whatsapp = bot()
+
+if __name__ == "__main__":
+    whatsapp
+    whatsapp.navegador()
+    whatsapp.site_whatsapp()
