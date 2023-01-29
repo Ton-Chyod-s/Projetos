31fa5bf52d4c086cdba1e2ad9cc401e4a530376d
userklay <arqkdias@gmail.com>
2023-01-29 14:52:40 -0400

    - ta indo

--- main_bot.py
+++ main_bot.py
@@ -1,6 +1,7 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
+import PySimpleGUI as sg
 import time
 
 class bot:
@@ -13,7 +14,29 @@ class bot:
     
 whatsapp = bot()
 
+class app:
+    def __init__(self):
+        selected_theme = 'Reddit'
+        sg.theme(selected_theme)
+
+        self.layout_login = [
+            [sg.Button('Web',size=(5,1)), sg.Text('INFRA',justification='c',size=(9,1)),sg.Button('Login',size=(6,1))],
+            [sg.Combo(['Firefox','Chrome','Internet Explorer','Edge']),sg.Checkbox('V-tal',key='home')],
+            ]
+
+        window = sg.Window('Tux-Netwin', icon='tux-natal.ico',layout=self.layout_login, keep_on_top=True, finalize = True,size=(250,75))
+
+        while True:
+            event,values = window.read()
+            if event == sg.WIN_CLOSED or event == 'Sair': # if user closes window or clicks cancel
+                break
+            
+            if event == 'Web':
+                whatsapp
+                whatsapp.navegador()
+                whatsapp.site_whatsapp()
+    
+        window.close()
+
 if __name__ == "__main__":
-    whatsapp
-    whatsapp.navegador()
-    whatsapp.site_whatsapp()
+    app()
