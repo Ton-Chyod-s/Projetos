db6524a0cbd798e51966316a65633a731728047d
Klay <arqkdias@gmail.com>
2023-04-08 08:38:29 -0400

    Update counter.py

--- counter.py
+++ counter.py
@@ -4,7 +4,7 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "to testando"
     page.add(
-        ft.OutlinedButton(text="Print",on_long_press=print('lol')),
+        ft.OutlinedButton(text="Print",on_click=print('lol')),
     )
 
 ft.app(target=main)
